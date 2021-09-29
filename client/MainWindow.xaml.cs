bc527bcc88a4189206f533163d7817cadafefc66
ly10951 <1095168483@qq.com>
2021-09-29 18:51:28 +0800

    Use style and template button

---
+++ client/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String text;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Init(object sender, RoutedEventArgs e)
+        {
+            text = "123";
+        }
+    }
+}
